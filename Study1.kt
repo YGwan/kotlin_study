ea661266ebb1e68cd5a85c6164e27e469a524964
YongGWAN <50222603+tjqldgkrl@users.noreply.github.com>
2021-09-30 03:14:39 +0900

    Update Study1.kt

--- Study1.kt
+++ Study1.kt
@@ -1,6 +1,6 @@
-//init관련 class
+//init가 있는 class
 
-class Init_Car(var model:String, var color:String){
+class Init_Car(var model:String, var color:String){//primary constructor
     fun getModelName():String{
         return model;
     }
@@ -8,11 +8,11 @@ class Init_Car(var model:String, var color:String){
         return color;
     }
     init{
-        println("${color} ${model} car created")
+        println("${color} ${model} car created") //init 블록 부분
     }
 }
 
-//constructor관련 class
+//secondary constructor가 있는 class
 
 class Constructor_Car {
     var model:String = ""
@@ -30,12 +30,26 @@ class Constructor_Car {
     }
 }
 
+//init, constructor 둘다 있는 class
+class InitWithConstructor{
+    init{
+        println("init created")
+    }
+
+    constructor(){
+        println("constructor created")
+    }
+
+}
+
 //main
 
 fun main(){
     var aventador = Init_Car("aventador","blue")
     var pony = Constructor_Car("pony","red")
-
     println("My first car was ${pony.model} ${pony.color}")
     println("My dream car was ${aventador.getModelName()} ${aventador.getCarColor()}")
+    println()
+    var sequence = InitWithConstructor()
+
 }
